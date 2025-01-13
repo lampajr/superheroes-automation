6ac166f4c91ded9bb584f5b171bb7672d2cb76c2
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-01-13 10:58:41 +0100

    Extract correct pid when entrypoint is a wrapper
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- modes/assets/semeru/start-quarkus.sh
+++ modes/assets/semeru/start-quarkus.sh
@@ -11,7 +11,7 @@ else
     mkdir cr
     $JAVA_HOME/bin/java -XX:CRaCCheckpointTo=cr -Dopenj9.internal.criu.unprivilegedMode=true ${JAVA_OPTS} ${JAVA_OPTS_APPEND} -jar ${JAVA_APP_JAR} 1>out 2>err </dev/null &
 
-    sleep 5
+    sleep 10
 
     if [[ "${ENDPOINT}" != "" ]]
     then
